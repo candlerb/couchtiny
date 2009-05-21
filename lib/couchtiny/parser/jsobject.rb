b0504e62f577e285a08aca964bc7b00563399deb
Brian Candler <B.Candler@pobox.com>
2009-05-21 15:25:24 +0100

    JSObject parser now supported natively by json-1.1.6

--- lib/couchtiny/parser/jsobject.rb
+++ lib/couchtiny/parser/jsobject.rb
@@ -3,29 +3,13 @@ require 'jsobject'
 
 module CouchTiny
   module Parser
+    # This parser requires json-1.1.6 or later
     module JSObject
       def parse(src)
-        tree = ::JSON.parse(src)
-        extend_jsobject(tree)
+        ::JSON.parse(src, :object_class => ::JSObject)
       end
       module_function :parse
 
-      # Walk a JSON object tree, extending all Hashes found with JSObjectMixin
-      def extend_jsobject(tree)
-        case tree
-        when Hash
-          tree.extend JSObjectMixin
-          tree.each do |k,v|
-            extend_jsobject(v)
-          end
-        when Array
-          tree.each do |v|
-            extend_jsobject(v)
-          end
-        end
-      end
-      module_function :extend_jsobject
-      
       def unparse(src)
         ::JSON.unparse(src)
       end
@@ -33,21 +17,3 @@ module CouchTiny
     end
   end
 end
-
-#module CouchTiny
-#  module Parser
-#    # This parser requires a patched version of the json library
-#    # but is more efficient
-#    module JSObject
-#      def parse(src)
-#        ::JSON.parse(src, :object_class => ::JSObject)
-#      end
-#      module_function :parse
-#
-#      def unparse(src)
-#        ::JSON.unparse(src)
-#      end
-#      module_function :unparse
-#    end
-#  end
-#end
